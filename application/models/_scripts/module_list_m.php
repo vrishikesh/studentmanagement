d2963773b67c31b1992048d1b982f41874e87a70
Riskey <riskey.it@gmail.com>
2018-10-01 23:09:00 +0530

    Created module tp populate list

---
+++ application/models/_scripts/module_list_m.php
@@ -0,0 +1,33 @@
+<?php
+
+class Module_list_m extends CI_Model {
+
+    public function insert_in_module_list( $files = [] ) {
+
+        $data = [];
+        foreach ($files as $file) {
+            $url = strtolower( $file );
+            if ( ! $this->check_if_exists( $url ) ) {
+                $name = explode('/', $url)[1];
+                $name = str_replace('_', ' ', $name );
+                $name = str_replace('-', ' ', $name );
+                $name = ucwords( $name );
+                $data[] = array(
+                    'NAME' => $name,
+                    'URL' => $url,
+                );
+            }
+        }
+        $this->db->insert_batch('module_list', $data);
+
+    }
+
+    public function check_if_exists( $url ) {
+
+        return $this->db
+                    ->where('URL', $url)
+                    ->count_all_results('module_list');
+
+    }
+
+}
